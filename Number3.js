e73f368a3caaa46e980484026a65a47b409909a9
Jorge Ceja-Carrillo <jceja-carrilio@csuchico.edu>
2016-11-04 19:55:33 -0700

    new files

---
+++ Number3.js
@@ -0,0 +1,44 @@
+var testThree = document.getElementById("buttonC");
+
+function answersNumber() {
+
+   var testOne =   Number(testOne);
+    if(isNaN(testOne))
+    { document.getElementById("invaildInput").classList.remove("hidden-message");
+    document.getElementById("invaildInput").classList.add("shown-message");
+    document.getElementById("invaildInput").innerHTML="Answer must be a number";
+    document.getElementById("invaildInput").classList.add("has-error");
+}
+else {
+setCookie("answerThree", testThree);
+
+
+}
+
+}
+function nextPromblem(){
+var newLink = document.getElementById("next");
+newLink.link("Test2.html");
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
